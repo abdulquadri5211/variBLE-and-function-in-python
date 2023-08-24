1aff9fbb369136ebfe9a845dd91d6f0103a7e978
abdul-quadri <abdulquadri7@gmail.com>
2023-08-24 11:59:51 +0100

    first commit

---
+++ Functions And Variables.py
@@ -0,0 +1,40 @@
+name = 'Jason' #Variable
+
+print('Hi. \'How \nare \nyou?') #
+print(name[2]) #
+print(name.upper()) #Make Content All Caps
+print(name.lower()) #Make Content LowerCase
+print(name.lower().islower()) #Asking If All Of Content Is LowerCase
+print(name.upper().isupper()) #Asking If All Of Content Is UpperCase
+print(len(name)) #Asking The Length of the Variable
+print(name.index('o')) #Asking For The Position Of Specified Character
+print(name.replace('o', 'a')) #Replacing Characters
+number = 79 #Variable
+number2 = str(number) #Changing Integer TO String
+print(78) #Print Integer
+print(78 + 22) #Print Simple Arithemetics Solutions
+print(number) #Print Variable
+print(number2) #Print Variable Containing Integer Turned String
+print(78 + 22.934) #Addition
+print(78 - 22.934) #Subtraction
+print(78 / 22.934) #Division
+print(78 * 22.934) #Multiplication
+print(20/6) #Whole Numbers Only
+print(20%6) #Remainder Only
+print('number is' + number) #Cannot Cocatenate String And Number
+print('number is' + number2) #Can Cocatenate Strings And String
+print(-5) #Print Negative Integer
+print(abs(-5)) #Absolute Function
+print(max(4,2,3,16)) #Print Highest Value
+print(min(4,2,3,16)) #Print Lowest Value
+print(round(3.2)) #Round Up To The Nearest Whole Integer
+print(round(3.5)) #Round Up To The Nearest Whole Integer
+print(bin(99)) #Binary Value Function
+print(bin(64684)) #Binary Value Function
+from math import * #To Import A Certain Set of Functions
+print(sqrt(34)) #Square Root Imported From "math"
+variable = input('String?\/String:') #Stores Users Input In A Variable
+print(variable) #Prints Input  
+names = input('Input Your Name: ')
+age = int(input('Input Your Age: ')) #
+print('Your name is ' + names + ' and you are ', age ,' years old') #E.g
